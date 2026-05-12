4f4eaf6bfaf94da73c07ea305795c3e851658773
SATYA NAGA PRAVEEN KUMAR NAMBURI <153361064+Satyanagapraveen@users.noreply.github.com>
2026-05-12 11:43:17 +0530

    Fix class name typo in User model and register User model in admin

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class user(AbstractUser):
+class User(AbstractUser):
         email=models.EmailField(max_length=255,unique=True)
         name=models.CharField(max_length=255, unique=True)
         avatar_url=models.URLField(max_length=200,blank=True,null=True)
